e436a9488a08e293c600176e9e5685dca1aed5b2
Francisco Perez Leiros <fran.pl@icloud.com>
2026-01-27 13:14:56 +0100

    Fix base path for GitHub Pages deployment to /new/

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/new/',
   plugins: [react()],
   resolve: {
     alias: {
