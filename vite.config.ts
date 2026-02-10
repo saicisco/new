7ea85329d562dd45ff1dfeb9ce339450faa98282
Francisco Perez Leiros <fran.pl@icloud.com>
2026-02-10 18:22:21 +0100

    Updated CV

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/new/',
+  base: '/portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
